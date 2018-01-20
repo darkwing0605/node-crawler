f80b18c922d59b0e84cf0a5c2ead20867d78feb0
darkwing <2553292615@qq.com>
2018-01-20 20:52:00 +0800

    20180120

--- crawler.js
+++ crawler.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
-
 request({
 	url: "https://www.dark-wing.com/",
 	method: "GET"
@@ -9,14 +8,11 @@ request({
 	if (e || !b) {return;}
 	const $ = cheerio.load(b);
 	let result = [];
-	let titles = $(".p");
-	// console.log(e);
-	// console.log(r);
-	// console.log(b);
-	// console.log($);
-	// console.log(titles)
+	let titles = $('a.post-title','.container');
 	for(var i = 0; i < titles.length; i++) {
-		result.push($titles[i].text());
+		let title = titles[i].children[0].data;
+		result.push(title);
 	}
-	fs.writeFileSync("result.json", JSON.stringify(result));
+	let results = JSON.stringify(result).replace(/(\\n)|(\s)/g, '');
+	fs.writeFileSync("result.json", results);
 });
